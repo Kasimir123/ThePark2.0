26531d233d90be9c16a74e53131ff0785acd4c3c
alexwohlbruck <alexwohlbruck@gmail.com>
2015-08-17 22:50:00 -0400

    Update init.js
    Custom options for dropdown menus

--- js/init.js
+++ js/init.js
@@ -22,3 +22,13 @@ $(document).ready( function() {
 $(document).ready(function(){
 	$('.modal-trigger').leanModal();
 });
+
+//Custom options for dropdown menus
+$('.dropdown-button').dropdown({
+    	inDuration: 300,
+    	outDuration: 225,
+    	constrain_width: false, // Does not change width of dropdown to that of the activator
+    	hover: true, // Activate on hover
+    	belowOrigin: false // Displays dropdown below the button
+    }
+);
