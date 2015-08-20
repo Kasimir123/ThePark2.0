2ef0e0dca1e88c3756962e1415fb9a48ab067d4b
Kasimir123 <kasimirkhschulz@gmail.com>
2015-08-20 16:40:34 -0400

    Update ajaxload.php

--- ajaxload.php
+++ ajaxload.php
@@ -2,12 +2,13 @@
 mysql_connect("mysql1.000webhost.com","a4028871_test2","test123") or die("Couldn't connect to sql server") ;
 mysql_select_db("a4028871_test2") or die("Couldn't select DB") ;
 
-$DB = mysql_query("SELECT * FROM messages ORDER BY id ASC LIMIT 20");
+$DB = mysql_query("SELECT * FROM messages ORDER BY id ASC");
 while ($get = mysql_fetch_assoc($DB)) {
 $get2 = $get['message'];
 $get3 = $get['username'];
 $chat = $get2;
 $username1 = $get3;
 echo '<div>' . '<br />' . $username1 . ': ' . $chat . '</div>';
+mysql_query("DELETE FROM messages WHERE added < (NOW() - INTERVAL 30 MINUTE)");
 }
 ?>
