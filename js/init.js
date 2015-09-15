621abcbb392e4d394b488d89753327f6eabee707
Kasimir123 <kasimirkhschulz@gmail.com>
2015-09-14 21:23:52 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,12 +8,17 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-//Initialize masonry
-$(document).ready( function() {
-	$('.grid').masonry({
-	itemSelector: '.grid-item',
-	columnWidth: 160,
-	isFitWidth: true,
-	gutter: 10
-	});
+//Initialize modals
+$(document).ready(function(){
+	$('.modal-trigger').leanModal();
 });
+
+//Custom options for dropdown menus
+$('.dropdown-button').dropdown({
+    	inDuration: 300,
+    	outDuration: 225,
+    	constrain_width: false, // Does not change width of dropdown to that of the activator
+    	hover: true, // Activate on hover
+    	belowOrigin: false // Displays dropdown below the button
+    }
+);
