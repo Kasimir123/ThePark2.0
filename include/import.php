3190e17eb24272b0642a7990c2fa447e87396f98
Kasimir123 <kasimirkhschulz@gmail.com>
2015-09-14 21:21:11 -0400

    Update import.php

--- include/import.php
+++ include/import.php
@@ -1,11 +1,13 @@
 <!-- Meta -->
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/><!-- Responsive -->
+<meta name="robots" content="The, Park, Myers, Park, High, School, Charlotte, NC, CMS, K-12" />
 
 <!-- CSS -->
 <link type="text/css" rel="stylesheet" href="materialize.min.css"  media="screen,projection"/><!-- Materialze CSS -->
 <link href="style.css" type="text/css" rel="stylesheet" media="screen,projection"/><!-- Override stylesheet -->
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"><!-- Material Icons -->
+<link href='https://fonts.googleapis.com/css?family=Product+Sans' rel='stylesheet' type='text/css'><!-- Product Sans font -->
 
 <!-- JS -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script><!-- jQuery -->
