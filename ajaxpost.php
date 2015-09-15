1211ab33ea6c13683c0e8e399705f4170afc70b7
Kasimir123 <kasimirkhschulz@gmail.com>
2015-09-14 21:03:53 -0400

    Update ajaxpost.php

--- ajaxpost.php
+++ ajaxpost.php
@@ -1,16 +1,14 @@
 <?php 
 
-	
-	include('include/dbcon.php');
-
-
+include('include/dbcon.php');
+include('include/functions.php');
 $user = $sesuser['username'];
 
-
-	$message = $_POST['messages'];
-	$time = date('Y-m-d H:i:s');
-	mysql_query("INSERT INTO messages VALUES ('','$message','$user','$time') ");
-	echo $message;
+$message = $_POST['messages'];
+$message = filterwords($message);
+$time = date('Y-m-d H:i:s');
+mysql_query("INSERT INTO messages VALUES ('','$message','$user','$time') ");
+echo $message;
 
 ?>
 
