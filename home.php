f76cb8b4b28f1612a1f22d2899fc1ac2fd4c0966
Kasimir123 <kasimirkhschulz@gmail.com>
2015-09-14 21:08:47 -0400

    Update home.php

--- home.php
+++ home.php
@@ -1,13 +1,13 @@
 <html>
 
 <head>
-<?php include('include/dbcon.php');?>
-<?php include('include/import.php');?>
+<?php include('./include/dbcon.php');?>
+<?php include('./include/import.php');?>
 </head>
 
 <body>
 
-<?php include('include/header.php');?>
+<?php include('./include/header.php');?>
 
 <p>As this site is under construction, please do not store any personal information as security is not guaranteed.</p>
 
